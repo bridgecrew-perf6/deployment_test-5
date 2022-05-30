770409f199f39668b14e8b19fc73b045b7dccca2
Farhan <mdfarhansamsuri@gmail.com>
2022-05-30 11:58:59 +0800

    added env

--- src/config/index.js
+++ src/config/index.js
@@ -4,12 +4,13 @@ const env = process.env.REACT_APP_ENV;
 //environment variables are variables that is used by the application itself
 //details such as secret keys/environment are part of the environment variables
 //in react, environment variables start with REACT_APP_
+console.log(env);
 
 let config = {
     baseURL : "https://localhost",
 }
 
-switch (env.toUpperCast()){
+switch (env.toUpperCase()){
     case "STAGE":
         config.baseURL="https://stage.skillsunion.com"
         break;
